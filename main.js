6d888af3b94c798f1c7cc2cc8096ab54442524cd
navya-shine <69377970+navya-shine@users.noreply.github.com>
2021-05-14 22:15:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("https://teachablemachine.withgoogle.com/models/fbq-E4b2M/model.json")
+function setup() {
+    canvas = createCanvas(300,230);
+    canvas.center();
+}
+function draw() {
+}
